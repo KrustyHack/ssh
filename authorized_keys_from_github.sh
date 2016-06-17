767b3842eec6d39dd71395db0f60f37efca29d29
KrustyHack <KrustyHack@users.noreply.github.com>
2016-06-17 15:13:36 +0200

    Create  authorized_keys_from_github.sh

---
+++ authorized_keys_from_github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget --quiet -O /tmp/keys https://github.com/krustyhack.keys
+for key in `cat /tmp/keys`; do
+	if grep --quiet $key /root/.ssh/authorized_keys; then
+		echo "Already registered"
+	else
+		echo $key >> /root/.ssh/authorized_keys
+	fi
+done
