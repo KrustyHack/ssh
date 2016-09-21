5b231b63f361d5d3f86459df86d86e70fe2c1d26
KrustyHack <KrustyHack@users.noreply.github.com>
2016-09-21 22:25:55 +0200

    Adding user account

--- authorized_keys_from_github.sh
+++ authorized_keys_from_github.sh
@@ -4,9 +4,16 @@ sudo wget --quiet -O /tmp/keys https://github.com/krustyhack.keys
 while read key; do
 	echo "--------------- $key ---------------"
 	if sudo grep --quiet "$key" /root/.ssh/authorized_keys; then
-		echo "Already registered"
+		echo "Already registered in root account"
 	else
-		echo "Key added"
+		echo "Key added in root account"
 		echo $key | sudo tee /root/.ssh/authorized_keys
+	fi
+
+	if sudo grep --quiet "$key" ~/.ssh/authorized_keys; then
+		echo "Already registered in user account"
+	else
+		echo "Key added in user account"
+		echo $key | sudo tee ~/.ssh/authorized_keys
 	fi
 done < "/tmp/keys"
