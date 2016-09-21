d985efeec30dd9b805d703f169b0709b56a971a1
KrustyHack <KrustyHack@users.noreply.github.com>
2016-09-21 22:17:54 +0200

    Update authorized_keys_from_github.sh

--- authorized_keys_from_github.sh
+++ authorized_keys_from_github.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget --quiet -O /tmp/keys https://github.com/krustyhack.keys
+sudo wget --quiet -O /tmp/keys https://github.com/krustyhack.keys
 while read key; do
 	echo "--------------- $key ---------------"
-	if grep --quiet "$key" /root/.ssh/authorized_keys; then
+	if sudo grep --quiet "$key" /root/.ssh/authorized_keys; then
 		echo "Already registered"
 	else
 		echo "Key added"
-		echo $key >> /root/.ssh/authorized_keys
+		sudo echo $key >> /root/.ssh/authorized_keys
 	fi
 done < "/tmp/keys"
