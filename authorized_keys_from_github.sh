f4eea26a2bb11a344bfddfbb070bb4ccfd744e9e
KrustyHack <KrustyHack@users.noreply.github.com>
2018-09-01 19:35:39 +0200

    Update authorized_keys_from_github.sh

--- authorized_keys_from_github.sh
+++ authorized_keys_from_github.sh
@@ -3,12 +3,6 @@
 sudo wget --quiet -O /tmp/keys https://github.com/krustyhack.keys
 while read key; do
 	echo "--------------- $key ---------------"
-	if sudo grep --quiet "$key" /root/.ssh/authorized_keys; then
-		echo "Already registered in root account"
-	else
-		echo "Key added in root account"
-		echo $key | sudo tee --append /root/.ssh/authorized_keys
-	fi
 
 	if sudo grep --quiet "$key" ~/.ssh/authorized_keys; then
 		echo "Already registered in user account"
